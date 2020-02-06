4fb582360ba971152223dcb04c145741842b68be
youngjun.jin <yzzzzun@gmail.com>
2020-02-06 21:33:20 +0900

    NewsList Layout 구현

---
+++ src/components/NewsList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const SampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160',
+};
+
+const NewsList = () => {
+  return (
+    <NewsListBlock>
+      <NewsItem article={SampleArticle}></NewsItem>
+      <NewsItem article={SampleArticle}></NewsItem>
+      <NewsItem article={SampleArticle}></NewsItem>
+      <NewsItem article={SampleArticle}></NewsItem>
+      <NewsItem article={SampleArticle}></NewsItem>
+      <NewsItem article={SampleArticle}></NewsItem>
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
