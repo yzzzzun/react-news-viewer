391b1d6981b146a4522900413964b89a0ab77e1f
youngjun.jin <yzzzzun@gmail.com>
2020-02-11 23:02:16 +0900

    카테고리 구성

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -23,7 +23,7 @@ const SampleArticle = {
   urlToImage: 'https://via.placeholder.com/160',
 };
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -31,8 +31,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=484ebe70f0864472a9c9db389dfec0e3',
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=484ebe70f0864472a9c9db389dfec0e3`,
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -41,7 +42,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   if (loading) {
     return <NewsListBlock>로딩중...</NewsListBlock>;
