fa1db6bcf1f4b91222d6c7813c2fb19aa47221ac
youngjun.jin <yzzzzun@gmail.com>
2020-02-18 21:12:09 +0900

    custom hook (usePromise) 구현으로 코드정리

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -16,42 +17,44 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const SampleArticle = {
-  title: '제목',
-  description: '내용',
-  url: 'https://google.com',
-  urlToImage: 'https://via.placeholder.com/160',
-};
-
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=484ebe70f0864472a9c9db389dfec0e3`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=484ebe70f0864472a9c9db389dfec0e3`,
+    );
   }, [category]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const query = category === 'all' ? '' : `&category=${category}`;
+  //       const response = await axios.get(
+  //         `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=484ebe70f0864472a9c9db389dfec0e3`,
+  //       );
+  //       setArticles(response.data.articles);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //     setLoading(false);
+  //   };
+  //   fetchData();
+  // }, [category]);
 
   if (loading) {
     return <NewsListBlock>로딩중...</NewsListBlock>;
   }
 
-  if (!articles) {
+  if (!response) {
     return null;
   }
 
+  if (error) {
+    return <NewsListBlock>에러발생</NewsListBlock>;
+  }
+
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map(article => (
